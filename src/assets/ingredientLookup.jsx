37b6fe151987f7475a0f50e55974e21001913017
Jacob Reese <jacobreese123@gmail.com>
2023-07-08 13:46:00 -0400

    access cocktail db and start implementation of fetch calls

---
+++ src/assets/ingredientLookup.jsx
@@ -0,0 +1,11 @@
+export const ingredients = [
+  {
+    idIngredient: "552",
+    strIngredient: "Elderflower cordial",
+    strDescription:
+      "Elderflower cordial is a soft drink made largely from a refined sugar and water solution and uses the flowers of the European elderberry. Historically the cordial has been popular in North Western Europe where it has a strong Victorian heritage.",
+    strType: "Cordial",
+    strAlcohol: "No",
+    strABV: null,
+  },
+];
--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,8 +1,32 @@
 import { Box, Typography } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
+import { useQuery } from "@tanstack/react-query";
 
 const Content = () => {
-  return <Typography sx={{ p: "1.6rem " }}>This is initial content</Typography>;
+  const [cocktail, setCocktail] = useState([]);
+
+  const drinkQuery = useQuery({
+    queryKey: ["drinks"],
+    queryFn: async () => {
+      const response = await fetch(
+        "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?iid=553"
+      );
+      const data = await response.json();
+      // console.log(data.ingredients);
+      setCocktail(data);
+      return data;
+    },
+  });
+
+  console.log(cocktail);
+
+  return (
+    <Box>
+      <Typography sx={{ p: "1.6rem " }}>
+        {JSON.stringify(cocktail.ingredients[0].strIngredient)}
+      </Typography>
+    </Box>
+  );
 };
 
 export default Content;
